917013fb845536001aaf81938f6c2c2696d145a3
samu7861 <sfrancog7861@gmail.com>
2023-08-11 10:02:37 -0500

    text box

--- rick_morty/lib/screens/home.dart
+++ rick_morty/lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/character_provider.dart';
+import '../models/character.dart'; // Asegúrate de importar tu modelo Character aquí
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,21 +9,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // Método para mostrar la información del personaje
+  void _showCharacterInfo(BuildContext context, Character character) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text(character.name),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.network(character.image),
+            SizedBox(height: 16),
+            Text('Status: ${character.status}'),
+            Text('Species: ${character.species}'),
+            Text('Type: ${character.type}'),
+            Text('Gender: ${character.gender}'),
+            Text('Origin: ${character.origin.name}'),
+            Text('Location: ${character.location.name}'),
+          ],
+        ),
+        actions: [
+          TextButton(
+            child: Text('Close'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('Rick and Morty Characters'),
+        elevation: 0,
         backgroundColor: Colors.transparent,
-        elevation: 5.0,
-        centerTitle: true,
-        title: Text(
-          'Rick and Morty Characters',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20.0,
-          ),
-        ),
       ),
+      backgroundColor: Color(0xFF2F1B4E),
       body: Column(
         children: <Widget>[
           const Padding(
@@ -43,26 +69,31 @@ class _HomeScreenState extends State<HomeScreen> {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return Center(child: CircularProgressIndicator());
                 } else if (snapshot.error != null) {
-                  return Center(
-                    child: Text(
-                      '¡Ups! Ha ocurrido un error al cargar los personajes. Inténtalo de nuevo más tarde.',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.redAccent),
-                    ),
-                  );
+                  return Center(child: Text('An error occurred!'));
                 } else {
                   return PageView.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: Provider.of<CharactersProvider>(context)
                         .characters
                         .length,
-                    itemBuilder: (ctx, i) => Container(
-                      width: MediaQuery.of(context).size.width,
-                      child: Image.network(
-                          Provider.of<CharactersProvider>(context)
+                    itemBuilder: (ctx, i) => GestureDetector(
+                      onTap: () {
+                        _showCharacterInfo(
+                            context,
+                            Provider.of<CharactersProvider>(context,
+                                    listen: false)
+                                .characters[i]);
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: Image.network(
+                          Provider.of<CharactersProvider>(context,
+                                  listen: false)
                               .characters[i]
                               .image,
-                          fit: BoxFit.cover),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                   );
                 }
