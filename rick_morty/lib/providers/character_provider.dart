5bd0c81387bb95dc0575a4a9a47e783ccaaf4a61
samu7861 <sfrancog7861@gmail.com>
2023-08-11 09:37:35 -0500

    Http excpetions

--- rick_morty/lib/providers/character_provider.dart
+++ rick_morty/lib/providers/character_provider.dart
@@ -10,10 +10,27 @@ class CharactersProvider with ChangeNotifier {
 
   Future<void> fetchCharacters() async {
     const url = 'https://rickandmortyapi.com/api/character';
-    final response = await http.get(Uri.parse(url));
-    final extractedData = json.decode(response.body)['results'] as List;
-    _characters =
-        extractedData.map((charJson) => Character.fromJson(charJson)).toList();
-    notifyListeners();
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> decodedData = json.decode(response.body);
+        if (decodedData.containsKey('results') &&
+            decodedData['results'] is List) {
+          final List<dynamic> characterList = decodedData['results'];
+          _characters = characterList
+              .map((charJson) => Character.fromJson(charJson))
+              .toList();
+          notifyListeners();
+        } else {
+          throw Exception('Invalid data format.');
+        }
+      } else {
+        throw Exception(
+            'Failed to load characters with status code ${response.statusCode}.');
+      }
+    } catch (error) {
+      print(error);
+      throw error;
+    }
   }
 }
